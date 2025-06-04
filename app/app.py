9f99c11f6e6506a5a685b24e5ba7b36cbf26e304
Andrew Close <andrewclose142@gmail.com>
2025-06-04 19:25:18 -0400

    feat: code for database created

--- app/app.py
+++ app/app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template, app
+from flask import Flask, render_template, request, abort
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object("config")
+# db = SQLAlchemy(app)
 
-@app.route("/")
+# class MessageModel(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     message = db.Column(db.String(875), nullable=False)
+#     sender = db.Column(db.String(30), nullable=False)
+#     date = db.Column(db.Date, nullable=False)
+
+# with app.app_context():
+#     db.create_all()
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        return render_template("index.html")
+    else:
+        abort(405)
 
 @app.route("/server-info")
 def server_info():
