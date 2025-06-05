de13ed6eb36ff1ccf180649ea2a5d17d3ffe8097
Andrew Close <andrewclose142@gmail.com>
2025-06-04 20:00:15 -0400

    feat: messaging with persistence implemented

--- app/app.py
+++ app/app.py
@@ -1,25 +1,39 @@
 from flask import Flask, render_template, request, abort
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_object("config")
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-# class MessageModel(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     message = db.Column(db.String(875), nullable=False)
-#     sender = db.Column(db.String(30), nullable=False)
-#     date = db.Column(db.Date, nullable=False)
+class MessageModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(875), nullable=False)
+    sender = db.Column(db.String(30), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    string_date = db.Column(db.String(40), nullable=False)
 
-# with app.app_context():
-#     db.create_all()
+with app.app_context():
+    db.create_all()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("index.html")
+        all_messages = db.session.execute(db.select(MessageModel).order_by(MessageModel.id)).scalars().all()
+        all_messages.reverse()
+        return render_template("index.html", all_messages=all_messages)
     elif request.method == "POST":
-        return render_template("index.html")
+        message = request.form.get("message")
+        sender = request.form.get("sender")
+        date = datetime.now()
+        string_date = datetime.strftime(date, "%B %#d, %Y, %#I:%M:%S %p")
+        message_model = MessageModel(message=message, sender=sender, date=date, string_date=string_date)
+        db.session.add(message_model)
+        db.session.commit()
+
+        all_messages = db.session.execute(db.select(MessageModel).order_by(MessageModel.id)).scalars().all()
+        all_messages.reverse()
+        return render_template("index.html", all_messages=all_messages)
     else:
         abort(405)
 
