6e6d567a726fff87bcb7df4f874d89fafa795e21
Andrew Close <andrewclose142@gmail.com>
2025-06-23 12:42:47 -0400

    feat: website design added

--- app/app.py
+++ app/app.py
@@ -4,41 +4,22 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_object("config")
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
-class MessageModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    message = db.Column(db.String(875), nullable=False)
-    sender = db.Column(db.String(30), nullable=False)
-    date = db.Column(db.Date, nullable=False)
-    string_date = db.Column(db.String(40), nullable=False)
-
-with app.app_context():
-    db.create_all()
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    if request.method == "GET":
-        all_messages = db.session.execute(db.select(MessageModel).order_by(MessageModel.id)).scalars().all()
-        all_messages.reverse()
-        return render_template("index.html", all_messages=all_messages)
-    elif request.method == "POST":
-        message = request.form.get("message")
-        sender = request.form.get("sender")
-        date = datetime.now()
-        string_date = datetime.strftime(date, "%B %#d, %Y, %#I:%M:%S %p")
-        message_model = MessageModel(message=message, sender=sender, date=date, string_date=string_date)
-        db.session.add(message_model)
-        db.session.commit()
+    return render_template("index.html")
 
-        all_messages = db.session.execute(db.select(MessageModel).order_by(MessageModel.id)).scalars().all()
-        all_messages.reverse()
-        return render_template("index.html", all_messages=all_messages)
-    else:
-        abort(405)
+@app.route("/bio")
+def bio():
+    return render_template("bio.html")
 
-@app.route("/server-info")
-def server_info():
-    return render_template("server-info.html")
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
 
 app.run()
