b4a0ae448c4a004e603eeeb593602307782dfcb8
Andrew Close <andrewclose142@gmail.com>
2025-06-27 22:59:19 -0400

    refactor: secret key made more secret

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ os.makedirs(INSTANCE_PATH, exist_ok=True)
 def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_PATH}"
-    app.config["SECRET_KEY"] = "secret-key"
+    app.config["SECRET_KEY"] = os.urandom(24)
 
     db.init_app(app)
 
